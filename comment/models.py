ea7ce7d060e07d3190043e9c7b41c535fac12d0f
Khilaar <luca.oliverio@hotmail.com>
2024-01-18 23:15:11 +0100

    First issues with models solved, db created but not checked if everything is correct yet

--- comment/models.py
+++ comment/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+
+
 User = get_user_model()
 
-
 class Comment(models.Model):
-    post = models.ForeignKey('Post', related_name='comment', on_delete=models.CASCADE)
+    post = models.ForeignKey('post.Post', related_name='post_to_user', on_delete=models.CASCADE)
     content = models.TextField()
-    author = models.ForeignKey('User', related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='comment_user_rel', on_delete=models.CASCADE)
     is_from_logged_in_user = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
