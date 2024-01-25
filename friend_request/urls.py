39475f25c838925acb71edab0e3c47d7876a87dd
Khilaar <luca.oliverio@hotmail.com>
2024-01-25 16:49:12 +0100

    Added view to get a single friendrequest. Added view to accept friend request. Added view to reject friend request

--- friend_request/urls.py
+++ friend_request/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import SendFriendRequestView, FriendRequestListView, SentFriendRequestListView, \
-    ReceivedFriendRequestListView
+    ReceivedFriendRequestListView, SingleSentFriendRequestView, AcceptFriendRequestView, RejectFriendRequestView
 
 urlpatterns = [
     path('request/<int:pk>/', SendFriendRequestView.as_view(), name='send-friend-request'),
-    path('allrequests', FriendRequestListView.as_view(), name='all-requests'),
-    path('sentrequests', SentFriendRequestListView.as_view(), name='sent-requests'),
-    path('receivedrequests', ReceivedFriendRequestListView.as_view(), name='received-requests')
+    path('allrequests/', FriendRequestListView.as_view(), name='all-requests'),
+    path('sentrequests/', SentFriendRequestListView.as_view(), name='sent-requests'),
+    path('receivedrequests/', ReceivedFriendRequestListView.as_view(), name='received-requests'),
+    path('singlerequest/<int:pk>/', SingleSentFriendRequestView.as_view(), name='single-request'),
+    path('accept/<int:pk>/', AcceptFriendRequestView.as_view(), name='accept-friend-request'),
+    path('reject/<int:pk>/', RejectFriendRequestView.as_view(), name='reject-friend-request'),
 ]
--- friend_request/views.py
+++ friend_request/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import FriendRequest
 from .serializers import FriendRequestSerializer
@@ -12,6 +14,7 @@ User = get_user_model()
 
 ########################################################################################
 
+#Send a friend request to a user
 #When we send a request in the frontend we need to add the requester id and the receiver id in the body. "requester" & "receiver"
 class SendFriendRequestView(CreateAPIView):
     queryset = FriendRequest.objects.all()
@@ -62,5 +65,36 @@ class ReceivedFriendRequestListView(ListAPIView):
     def get_queryset(self):
         current_user = self.request.user
         return FriendRequest.objects.filter(receiver=current_user)
+
+########################################################################################
+
+#Get a single friend request
+class SingleSentFriendRequestView(RetrieveAPIView):
+    queryset = FriendRequest.objects.all()
+    serializer_class = FriendRequestSerializer
+
+    def get_object(self):
+        friend_request_id = self.kwargs['pk']
+        return get_object_or_404(FriendRequest, id=friend_request_id)
+
+########################################################################################
+
+#Accept a friend request
+class AcceptFriendRequestView(APIView):
+    def post(self, request, pk):
+        friend_request = get_object_or_404(FriendRequest, id=pk)
+        friend_request.status = FriendRequest.ACCEPTED
+        friend_request.save()
+        return Response({'detail': 'Friend request accepted.'})
+
+########################################################################################
+
+#Reject a friend request
+class RejectFriendRequestView(APIView):
+    def post(self, request, pk):
+        friend_request = get_object_or_404(FriendRequest, id=pk)
+        friend_request.status = FriendRequest.REJECTED
+        friend_request.save()
+        return Response({'detail': 'Friend request rejected.'})
 
 ########################################################################################
